fe5cda695320f31fc82373f7686752352bef88f7
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-10-07 18:07:21 +0200

    Add basics exercises

---
+++ src/basics/exercises.js
@@ -0,0 +1,13 @@
+// Scrivi una funzione javascript per clonare un array (hint immutabilita')
+
+// Scrivi una funzione javascript per ritornare un sotto array dei primi n elementi
+
+// Scrivi una funzione javascript per eliminare elementi duplicati in un array (hint Set)
+
+// Scrivi una funzione javascript che ritorni un oggetto contatore
+// con dei metodi per incrementarlo, decrementarlo e stamparlo (RULE no class, metodi su oggetto)
+
+// Scrivi una funzione che iteri su un array e ne stampi gli elementi
+
+// Scrivi una funzione javascript che unisca due dizionari/oggetti (hint spread operator)
+
